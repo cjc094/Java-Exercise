3c44bf4cd4fa78f4bc06622cd755090b14513700
wwilsson94624 <01257150@mail.ntou.edu.tw>
2025-04-24 14:30:50 +0800

    feat: third version-print the third member name

--- FirstClass3Team.java
+++ FirstClass3Team.java
@@ -3,5 +3,6 @@ public static void main(String[] args) {
     System.out.println("這是用來練習GitHub協作的程式!");
     System.out.println("\"學號：\"01257157\"\n姓名：\"陳婕熙\"");
     System.out.println("\"學號：\"01257117\"\n姓名：\"呂翰昇\"");
+    System.out.println("\"學號：\"01257150\"\n姓名：\"劉耀升\"");
     }
 }
